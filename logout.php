4c5e0103be59567dff400455a927415b198398c5
giupino82 <giupino.garofalo@gmail.com>
2017-06-23 17:15:41 +0200

    Corretto redirect
    Corretto redirect

--- logout.php
+++ logout.php
@@ -1,52 +1,13 @@
 <?php
 	session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hattrick Radar</title>
-
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/responsive.css">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-  <?php
-	if (isset($_COOKIE['htRadarOauthToken'])) {
+    	if (isset($_COOKIE['htRadarOauthToken'])) {
 		unset($_COOKIE['htRadarOauthToken']);
 		unset($_COOKIE['htRadarOauthTokenSecret']);
 		setcookie('htRadarOauthToken', null, -1, '/');
 		setcookie('htRadarOauthTokenSecret', null, -1, '/');
 		unset($_SESSION['tmpToken']);
 		unset($_SESSION['HT']);
-
-		echo '<p>Logout ok!</p>';
-		echo '<p>Redirecting...</p>';
-		sleep(2);
-		header('Location: http://localhost/hattrickradar/');
-
 		}
-	?>
-  </body>
+	header("Location: index.php");
+	exit();
+?>
